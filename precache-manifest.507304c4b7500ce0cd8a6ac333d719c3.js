1af610b85507b5207414e1aa60c4b58deb8bec3a
devdahcoder <adigunolamide200@gmail.com>
2020-09-21 20:35:38 +0100

    Updates

---
+++ precache-manifest.507304c4b7500ce0cd8a6ac333d719c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28bfebe25fa5ed930c1425df0a0869f",
+    "url": "/momentumclone/index.html"
+  },
+  {
+    "revision": "fa409da62a17c1847941",
+    "url": "/momentumclone/static/css/main.fb52181e.chunk.css"
+  },
+  {
+    "revision": "a3e6b4b974e9b1c1e8b6",
+    "url": "/momentumclone/static/js/2.0ee85c9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/momentumclone/static/js/2.0ee85c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa409da62a17c1847941",
+    "url": "/momentumclone/static/js/main.3c797486.chunk.js"
+  },
+  {
+    "revision": "729a9f677c385dbf1328",
+    "url": "/momentumclone/static/js/runtime-main.d7912705.js"
+  }
+]);
