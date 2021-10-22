2aa086245c28f5848414ece299f171e79c1c2eac
devdhacoder <adigunolamide200@gmail.com>
2021-10-22 17:41:02 +0100

    Refator: changed daily forcast weather icon position for more visibility

--- src/momentumComponents/WeatherDailyItem/WeatherDailyItem.js
+++ src/momentumComponents/WeatherDailyItem/WeatherDailyItem.js
@@ -33,10 +33,10 @@ const WeatherDailyItem = ({day, index}) => {
     return (
         <div onClick={() => handleCurrentWeatherDropDown(day, index)} ref={elementRef} id={`${activeLink === index ? "selected" : ""}`} className="weather-forecast-item weather-forecast-day" data-day="Friday" data-test="forecast-day" title={day && day.Day.IconPhrase}>
             <div className="daily-weather-forecast-label" data-test="forecast-day-label">{DateToDay(day)}</div>
+            <span className="icon icon-weather weather-forecast-icon" data-icon="H" title={day && day.Day.IconPhrase}>
+                {renderIcon()}  
+            </span>
             <div className="daily-weather-forecast-day-details">
-                <span className="icon icon-weather weather-forecast-icon" data-icon="H" title={day && day.Day.IconPhrase}>
-                    {renderIcon()}  
-                </span>
                 <span className="weather-forecast-high" data-test="forecast-high">{day.Temperature.Maximum.Value.toFixed()}°</span>
                 <span className="weather-forecast-low">{day.Temperature.Minimum.Value.toFixed()}°</span>
             </div>
